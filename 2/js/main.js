d1db61a29837be7cb4fb4d08240acbd35534d3a1
Keks <keks@htmlacademy.ru>
2022-11-28 19:00:23 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,7 +1,10 @@
-function getRand(min, max) {
+function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function checkLen(string, max) {
+function checkStringLenth(string, max) {
   return string.length <= max;
 }
+
+console.log(getRandomNumber(1, 10))
+console.log(checkStringLenth('sdfs', 10));
