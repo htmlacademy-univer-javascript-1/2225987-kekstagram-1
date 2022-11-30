3e0d726f16d7cfa538c32fac1900bb40f3a18cf7
Egor09102003 <42377292+Egor09102003@users.noreply.github.com>
2022-11-30 15:55:49 +0500

    Add files via upload

--- 2225987-kekstagram-1/js/util.js
+++ 2225987-kekstagram-1/js/util.js
@@ -1,13 +1,28 @@
+//Генерация числа из массива
+
 function getRandomPositiveInteger (a, b) {
-    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-    const result = Math.random() * (upper - lower + 1) + lower;
-    return Math.floor(result);
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+// Функция для проверки максимальной длины строки.
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+checkStringLength('строка', 5);
+
+// Функция закрытия окна по Escape
+
+const getEscapeEvent = (evt, action) => {
+  if (evt.key === 'Escape') {
+    action();
   }
-  
-  const getRandomArrayElement = (elements) => {
-    return elements[getRandomPositiveInteger(0, elements.length - 1)];
-  };
-  
-  export {getRandomArrayElement};
-  export {getRandomPositiveInteger};
+};
+
+export {getRandomPositiveInteger};
+export {checkStringLength};
+export {getEscapeEvent};
