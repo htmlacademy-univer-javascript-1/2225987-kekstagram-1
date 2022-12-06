f404a156523f1b1a0a665e421f889d16e512fa80
Egor09102003 <42377292+Egor09102003@users.noreply.github.com>
2022-12-06 14:05:50 +0500

    Project

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,34 +1,16 @@
 import {isEscapeKey} from './util.js';
 
+const MAX_COMMENTS_TO_SHOW = 5;
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
-const commentsCount = bigPicture.querySelector('.comments-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const commentsContainer = bigPicture.querySelector('.social__comments');
+const commentsToShowCount = bigPicture.querySelector('.social__comment-count');
 const body = document.querySelector('body');
-
-const onPopupEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeBigPicture ();
-  }
-};
-
-const onPopupCloseButtonClick = () => {
-  closeBigPicture();
-};
-
-function closeBigPicture () {
-  bigPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-  commentsCount.classList.remove('hidden');
-  commentsLoader.classList.remove('hidden');
-  document.removeEventListener('keydown', onPopupEscKeydown);
-  document.removeEventListener('click', onPopupCloseButtonClick);
-}
+let count = 0;
 
 const createCommentItem = (comment) => {
-  const newCommentItem  = document.createElement('li');
+  const newCommentItem = document.createElement('li');
   newCommentItem.classList.add('social__comment');
   const commentImage = document.createElement('img');
   commentImage.classList.add('social__picture');
@@ -45,26 +27,56 @@ const createCommentItem = (comment) => {
   return newCommentItem;
 };
 
-const renderComments = (comments) => {
-  commentsContainer.innerHTML = '';
-  const commentsFragment = document.createDocumentFragment();
-  comments.forEach((comment) => {
-    commentsFragment.appendChild(createCommentItem(comment));
-  });
-  commentsContainer.appendChild(commentsFragment);
-};
-
 const showBigPicture = (picture) => {
+
+  const onPopupEscKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      closeBigPicture();
+    }
+  };
+
+  const onPopupCloseButtonClick = () => {
+    closeBigPicture();
+  };
+
+  function closeBigPicture() {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+    document.removeEventListener('keydown', onPopupEscKeydown);
+    document.removeEventListener('click', onPopupCloseButtonClick);
+    commentsLoader.removeEventListener('click', commentsLoaderOnClick);
+    count = 0;
+  }
+
+  function commentsLoaderOnClick() {
+    // изменяем значение count прибавляя 5, следовательно slice станет (5, 10), отрисуется еще 5 штук
+    count += MAX_COMMENTS_TO_SHOW;
+    renderCommentsSlice();
+  }
+
+  function renderCommentsSlice() {
+    commentsContainer.innerHTML = '';
+    const commentsFragment = document.createDocumentFragment();
+    // создаем срез комментов, будет показываться 5 штук, при клике count перезапишется
+    const commentsToShow = picture.comments.slice(0, count + MAX_COMMENTS_TO_SHOW);
+    commentsToShow.forEach((comment) => {
+      commentsFragment.appendChild(createCommentItem(comment));
+    });
+    commentsContainer.appendChild(commentsFragment);
+    commentsLoader.classList.toggle('hidden', picture.comments.length === commentsToShow.length);
+    commentsToShowCount.innerHTML = `${commentsToShow.length} из <span class="comments-count">${picture.comments.length}</span> комментариев`;
+  }
+
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
-  commentsCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   bigPicture.querySelector('.big-picture__img img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
 
-  renderComments(picture.comments);
+  renderCommentsSlice();
+  commentsLoader.addEventListener('click', commentsLoaderOnClick);
 
   closeButton.addEventListener('click', onPopupCloseButtonClick);
   document.addEventListener('keydown', onPopupEscKeydown);
